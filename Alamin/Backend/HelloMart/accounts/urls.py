570540785f69e5d600b306c9e5f2ae9e56b22594
MDalamin5 <mdal.amin5@northsouth.edu>
2024-03-25 23:07:08 +0600

    add templates in cart apps

--- Alamin/Backend/HelloMart/accounts/urls.py
+++ Alamin/Backend/HelloMart/accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register)
+    path('register/', views.register),
+    path('signin/', views.user_login),
+    path('profile/', views.profile)
 ]
--- Alamin/Backend/HelloMart/accounts/views.py
+++ Alamin/Backend/HelloMart/accounts/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def register(request):
     return render(request, 'accounts/register.html')
+
+def user_login(request):
+    return render(request, 'accounts/signin.html')
+
+def profile(request):
+    return render(request, 'accounts/dashboard.html')
