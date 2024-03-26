05cbe5ca22516563d118a4d89a6a8851a96624c7
MDalamin5 <mdal.amin5@northsouth.edu>
2024-03-27 00:04:42 +0600

    product and category table creation and media file handeling from backend

--- Alamin/Backend/HelloMart/store/admin.py
+++ Alamin/Backend/HelloMart/store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import Product
 
 # Register your models here.
+# admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):  # for customization use Model Admin
+    prepopulated_fields = {'slug' : ('product_name',)}
+    list_display = ('product_name', 'price', 'category', 'stock', 'created_date', 'modified_date', 'is_available')
+
+admin.site.register(Product, ProductAdmin)
