c4caa9da83e0be0115c87279a9205ebb9f08fd34
MDalamin5 <mdal.amin5@northsouth.edu>
2024-03-28 22:32:29 +0600

    product filter by category

--- Alamin/Backend/HelloMart/store/urls.py
+++ Alamin/Backend/HelloMart/store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
+    path('category/<slug:category_slug>/', views.store, name='products_by_category'),
     path('product_detail/', views.productDetail, name='product_detail'),
 ]
--- Alamin/Backend/HelloMart/store/views.py
+++ Alamin/Backend/HelloMart/store/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Product
+from category.models import Category
+
 
 # Create your views here.
-def store(request):
-    products = Product.objects.filter(is_available = True)
+def store(request, category_slug=None):
+    category = None
+    products = None
+    categories = Category.objects.all()
     
+    if category_slug:
+        category = get_object_or_404(Category, slug = category_slug)
+        products = Product.objects.filter(is_available = True, category = category)
+    else:
+        products = Product.objects.filter(is_available = True)
+    
+    # print(categories)
     # for item in products: # unit-test code
     #     print(item.product_name, item.price, item.is_available)
     
-    return render(request, 'store/store.html', {'products' : products})
+    context = {'products' : products, 'categories' : categories}
+    return render(request, 'store/store.html', context)
 
 def productDetail(request):
     return render(request, 'store/product-detail.html')
