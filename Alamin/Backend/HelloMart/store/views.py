c79b1195c7dc49d3b93f4f43b41d5235d3598a0c
MDalamin5 <mdal.amin5@northsouth.edu>
2024-03-28 15:38:22 +0600

    add product from backend and show in frontend

--- Alamin/Backend/HelloMart/store/views.py
+++ Alamin/Backend/HelloMart/store/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from . models import Product
 
 # Create your views here.
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.filter(is_available = True)
+    
+    # for item in products: # unit-test code
+    #     print(item.product_name, item.price, item.is_available)
+    
+    return render(request, 'store/store.html', {'products' : products})
 
 def productDetail(request):
     return render(request, 'store/product-detail.html')
