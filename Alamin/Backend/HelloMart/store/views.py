0f76927ea8858291d1ca011d53378d06544ccb41
MDalamin5 <mdal.amin5@northsouth.edu>
2024-03-29 12:02:32 +0600

    paginations done

--- Alamin/Backend/HelloMart/store/views.py
+++ Alamin/Backend/HelloMart/store/views.py
@@ -12,22 +12,31 @@ def store(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug = category_slug)
         products = Product.objects.filter(is_available = True, category = category)
+        page = request.GET.get('page')
+        print(page)
         paginator = Paginator(products, 1)
+        paged_product = paginator.get_page(page)
         
     else:
         products = Product.objects.filter(is_available = True)
+        page = request.GET.get('page')
         paginator = Paginator(products, 2)
-        paged_product = paginator.get_page(1)
+        paged_product = paginator.get_page(page)
         
-        for i in paged_product:
-            print(i)
+        # for i in paged_product:
+        #     print(i)
+    print(paged_product.has_next(), paged_product.has_previous(), paged_product.previous_page_number, paged_product.next_page_number)
 
     
-    # paginator Section
+    """Paginator add using random blog
+
+    Returns:
+        Source: https://testdriven.io/blog/django-pagination/
+    """
     
     
     
-    context = {'products' : products, 'categories' : categories}
+    context = {'products' : paged_product, 'categories' : categories}
     return render(request, 'store/store.html', context)
 
 def productDetail(request):
