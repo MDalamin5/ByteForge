00b3c7cc7e9c0a682c3b81ba3b4b8bd6b6b9e491
mdalamin5 <mdal.amin5@northsouth.edu>
2024-04-06 14:25:04 +0600

    registaration, login and logout done

--- Alamin/Backend/HelloMart/accounts/urls.py
+++ Alamin/Backend/HelloMart/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('signin/', views.user_login, name='login'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- Alamin/Backend/HelloMart/accounts/views.py
+++ Alamin/Backend/HelloMart/accounts/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data.get('first_name'))
+            user = form.save()
+            login(request, user)
+            
+            
+            return redirect('cart')
+            
+    
+    
+    return render(request, 'accounts/register.html', {'form' : form})
 
 def user_login(request):
+    if request.method == "POST":
+        # print(request.POST)
+        user_name = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username = user_name, password = password)
+        print(user)
+        login(request, user)
+        return redirect('profile')
     return render(request, 'accounts/signin.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 def profile(request):
     return render(request, 'accounts/dashboard.html')
+
+
+
+
+
