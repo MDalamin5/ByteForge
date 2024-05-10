0ccbb81a1fb509ee7dfe7a1bf282bc282f3ab9ae
MDalamin5 <mdal.amin5@northsouth.edu>
2024-05-10 08:47:11 +0600

    add ssl commerce store id

--- Alamin/Backend/HelloMart/orders/models.py
+++ Alamin/Backend/HelloMart/orders/models.py
@@ -49,3 +49,7 @@ class OrderProduct(models.Model):
     ordered = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     
+    
+class PaymentGateWaySettings(models.Model):
+    store_id = models.CharField(max_length=200, blank=True, null=True)
+    store_pass = models.CharField(max_length=400, blank=True, null=True)
